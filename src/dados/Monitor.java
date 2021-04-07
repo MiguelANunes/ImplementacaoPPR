9f1826e4e06a5edcf6e28fb97346c2c4e7f6048b
EduardoPandini <70462380+EduardoPandini@users.noreply.github.com>
2021-04-06 22:12:06 -0300

    Add files via upload
    fun arrays now can be deleted and listed orderly OwO

--- src/dados/Monitor.java
+++ src/dados/Monitor.java
@@ -3,31 +3,64 @@ package dados;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Monitor extends Pessoa {
-    private float score; 
+public class Monitor {
+    private String nome, cpf;
+    private Materia materia;
+    private float totalscore=0,score; 
     private List<Avaliacao> listaAvaliacao = new ArrayList();
     private int avaliacaoCount=0;
+    
 
-    public Monitor(String nome, String cpf, String senha, Materia materia, float score) {
-        super(nome,cpf,senha);
+    public Monitor(String nome, String cpf, Materia materia, float score, List<Avaliacao> avaliacao) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.materia = materia;
         this.score = score;
-    }
-    
-    public Monitor(){
-    
+        this.listaAvaliacao = avaliacao;
     }
 
     public void AdicionaAvaliacao(Avaliacao a) {
 		listaAvaliacao.add(a);
 		avaliacaoCount++;
+		totalscore = totalscore+= a.getRanking();
+		setScore(totalscore/avaliacaoCount);
 	}
+    public void removeAvaliacao(Avaliacao a){
+        listaAvaliacao.remove(a);
+        avaliacaoCount--;
+    }
+    public List<Avaliacao> consultaAvaliacao(){
+        List<Avaliacao> listaRetorno = new ArrayList(listaAvaliacao);
+        return listaRetorno;
+    }
+	public String getNome() {
+        return nome;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
 
     public float getScore() {
         return score;
     }
 
- 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
     public void setScore(float score) {
         this.score = score;
     }
@@ -35,15 +68,15 @@ public class Monitor extends Pessoa {
     public void login(){
     // void por hora    
     }
-
+    
     public void logout(){
     // void por hora
     }
 
     @Override
     public String toString() {
-        return "Monitor{" + "nome: " + nome + ", cpf: " + cpf + "score:"  + score + '}';
+        return "Monitor{" + "nome: " + nome + ", cpf: " + cpf + ", materia: " + materia + ", score: " + score + '}';
     }
 
-
+    
 }
